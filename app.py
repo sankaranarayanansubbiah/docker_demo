0170454356ab00839c63bc10e03bb00a8c7f854f
sankaranarayanan <163542734+sankaranarayanansubbiah@users.noreply.github.com>
2024-04-04 09:56:49 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import flask
+from flask import Flask
 
-app =Flask(_name_)
+app = Flask(__name__)
 
-@app.router('/')
+@app.route('/')
 def index():
-    return '<h1>My Basic Details</h1><p>Name:Sankar</p><p>Email:austinsankar@gmail.com</p>'
+    return '<h1>My Basic Details</h1><p>Name: Sankar</p><p>Email: austinsankar@gmail.com</p>'
 
-    if _name_=='_main_':app.run(host='0.0.0.0',port=5000)
-    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
